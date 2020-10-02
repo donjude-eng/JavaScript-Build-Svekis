5b69f1de4e9f8d988410b97a4da7336696ed26d6
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-10-02 14:58:22 +0530

    Updated the object lesson

--- JS-Bible-Bootcamp2020/index.js
+++ JS-Bible-Bootcamp2020/index.js
@@ -1,5 +1,5 @@
 //EXAMPLE 1 - "const"
-const myBoolean = true;
+//const myBoolean = true;
 //console.log(myBoolean)
 
 /**
@@ -10,66 +10,196 @@ const myBoolean = true;
  * NOTE: vraiable "myObject" Will NOT be reassigned in the future.
  */
 
- //const myObject = {};
- //console.log(myObject);
+//const myObject = {};
+//console.log(myObject);
 
- //myObject = {};
+//myObject = {};
 
- /**
-  * CHALLENGE 2
-  * Declare "x" and assign value 10 to it.
-  * Declare "y" and assign value true in it.
-  * Declare "myObject" and assign object with two name-value pairs.
-  * Declare "anotherObject".
-  * Later assign value
-  */
+/**
+ * CHALLENGE 2
+ * Declare "x" and assign value 10 to it.
+ * Declare "y" and assign value true in it.
+ * Declare "myObject" and assign object with two name-value pairs.
+ * Declare "anotherObject".
+ * Later assign value
+ */
 
-let x = 10;
-const y = true;
-const myObject = {
-    a:x,
-    b:y
-}
-//console.log(myObject)
+// let x = 10;
+// const y = true;
+// const myObject = {
+//     a:x,
+//     b:y
+// }
+// //console.log(myObject)
 
-//Another object
-x = 20;
-let anotherObject;
-anotherObject = {
-    newA:x,
-    b: y,
-    c: myObject
-};
+// //Another object
+// x = 20;
+// let anotherObject;
+// anotherObject = {
+//     newA:x,
+//     b: y,
+//     c: myObject
+// };
 //console.log(anotherObject)
 
-
 //Dynamic typing in javascript
-let myVariable;
+// let myVariable;
 
-//console.log(myVariable); //Undefined
+// //console.log(myVariable); //Undefined
 
-myVariable = 10;
+// myVariable = 10;
 
-//console.log(myVariable); //10
+// //console.log(myVariable); //10
 
-myVariable = true;
+// myVariable = true;
 
-//console.log(myVariable); //true
+// //console.log(myVariable); //true
 
-myVariable = {
-    x: true,
-    y: 10
+// myVariable = {
+//     x: true,
+//     y: 10
+// };
+
+// //console.log(typeof myVariable) //{}
+
+// // CHALLANGE
+
+// let myname = null;
+// //onsole.log(typeof myname);
+
+// myName = 15;
+// //console.log(typeof myname);
+
+// myName = false;
+//console.log(typeof myName);
+
+//Object example
+
+// const myCity = {
+//     city: "Pulpally",
+//     state : "Kerala",
+//     popular : true,
+//     numberOfDist : 14
+// }
+
+// myCity.city = "Bathery";
+
+// console.log(myCity.city)
+
+// const mycity = {
+//     city : "Pulpally",
+// }
+// mycity["Popular"] = true;
+// console.log(mycity)
+
+// let myObject;
+
+// myObject = {
+//     a: 10,
+//     b: "abc"
+// }
+
+// console.log(myObject);
+
+// myObject.a = 15;
+// console.log(myObject);
+
+// myObject.c = true;
+// console.log(myObject)
+
+// delete myObject.b;
+// console.log(myObject)
+
+/**
+ * CHANLLENGE 2;
+ *
+ * Create a varaible called "myPost".
+ * Initial value should be {}.
+ * Add property called "postTitle" and value "Object is reference type"
+ * Add one more property "postLokes" with value 0.
+ * Add third property "shared" and set its value 0
+ * Incearse value of the "Postlikes" by 1
+ * Delete property shared
+ */
+
+//  let myPost = {};
+
+//  myPost = {
+//      postTitle : "",
+//      postLike : 0,
+//      shared: false,
+//  }
+// console.log(myPost)
+
+//  myPost.postLike = 1;
+//  console.log(myPost)
+
+//  delete myPost.shared;
+
+// let myObject = {"a" : -10}
+// let copyOfMyObject = myObject;
+
+// console.log(copyOfMyObject)
+
+// copyOfMyObject.b = false;
+// console.log(copyOfMyObject)
+// console.log(myObject)
+
+/**
+ * EXAMPLE 3
+ */
+
+//  let myObject = {
+//      a: true,
+//      b: null,
+//      c: 25
+//  }
+
+//  const propertyName = "c";
+//  console.log(myObject[propertyName]);
+//  console.log(myObject["propertyName"]);
+//  myObject["new" + "Property" + "Name" ] = "Don jude Joseph"
+//  console.log(myObject)
+
+// //  console.log(myObject["a"])
+
+// //  console.log(myObject["b"])
+
+// //  console.log(myObject["c"])
+
+//  //myObject["Name"] = "Don Jude";
+
+/**
+ * Example 4
+ * Missing properties
+ */
+
+const myObject = {
+  a: 3,
+  b: true,
 };
+//myObject.
 
-//console.log(typeof myVariable) //{}
+//console.log(myObject.c) //Undefined
 
-// CHALLANGE
 
-let myname = null;
-console.log(typeof myname);
+/**
+ * CHALLENGE 3
+ * "objectWithNestedObject" with initial vale {}.
+ * Add property "Nested object" iniial vale{}
+ * Add property "a" with value "null" to "nestedObject" . Use bracket
+ * notation
+ * Create new variable with the property name
+ *
+ */
 
-myName = 15;
-console.log(typeof myname);
+ let objectwithNestedObject = {};
 
-myName = false;
-console.log(typeof myName);
+objectwithNestedObject.nestedObject = {};
+
+objectwithNestedObject.nestedObject.a = null;
+
+const newPropertName = "b";
+
+objectwithNestedObject.nestedObject[newPropertName] = "Don Jude";
+console.log(objectwithNestedObject)
