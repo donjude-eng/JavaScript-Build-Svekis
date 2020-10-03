c9489f560798e13d725f5fddff48e9e014f111d4
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-10-03 21:01:52 +0530

    Code added

---
+++ JS_core_fundamentals/lesson-1/chap.index.js
@@ -0,0 +1,62 @@
+// let x =10;
+// console.log(x -5);
+// x = 20;
+// console.log(x + x);
+// let name = "Don Jude";
+// console.log(name)
+// name = "Akash";
+// console.log(name)
+
+// // Challenge Part
+// var a = "hello world";
+// console.log(a);
+// a = "Welcome Back";
+// console.log(a);
+
+// let myName = "Don Jude";
+// console.log(myName);
+// myName = "Rajashekaran";
+// console.log(myName);
+
+
+// Challenge Part (Questions : 2)
+
+// var message;
+// console.log(message);
+// message = null;
+// console.log(message);
+// var myLight = false;
+// console.log(myLight);
+// myLight = true;
+
+// if(myLight === true) {
+//     console.log(myLight);
+// }
+
+// var score1, score2,score3,score4;
+// var a = "Hello";
+// var b = 10;
+// var c = false;
+// console.log(a);
+
+// // Let message
+// let message;
+// console.log(message);
+
+// message = null;
+
+// if(true) {
+//     let message = "updated message";
+//     console.log(message);
+// }
+
+// console.log(message)
+
+// /// //Lesson Challenge
+let varA = "Don";
+let varB = "Jude";
+let varC = "Joseph"
+
+console.log(varA);
+console.log(varB);
+console.log(varC);
