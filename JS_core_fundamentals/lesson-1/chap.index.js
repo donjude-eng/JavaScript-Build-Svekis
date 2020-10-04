00128f844323a43f26785a317f260a91754f5435
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-10-04 21:11:30 +0530

    JS lessons

--- JS_core_fundamentals/lesson-1/chap.index.js
+++ JS_core_fundamentals/lesson-1/chap.index.js
@@ -18,9 +18,7 @@
 // myName = "Rajashekaran";
 // console.log(myName);
 
-
-// Challenge Part (Questions : 2)
-
+// // // Challenge Part (Questions : 2)
 // var message;
 // console.log(message);
 // message = null;
@@ -52,11 +50,52 @@
 
 // console.log(message)
 
-// /// //Lesson Challenge
-let varA = "Don";
-let varB = "Jude";
-let varC = "Joseph"
+/// /// /// Lesson Challenge
 
-console.log(varA);
-console.log(varB);
-console.log(varC);
+// let varA = "Don";
+// let varB = "Jude";
+// let varC = "Joseph"
+
+// console.log(varA);
+// console.log(varB);
+// console.log(varC);
+
+/// /// /// Code Snippet
+// const test = true;
+// let message = "Welcome to Javascript";
+// let moreStuff;
+// if(test) {
+//     console.log(message);
+//     moreStuff = "Yes the content is there";
+//     let inBlock = "This is a secret";
+//     console.log(inBlock);
+// }
+// console.log(moreStuff)
+
+
+/// // /// Test Challenge
+// const name = true;
+// let moreName;
+// let message = "Welcome to the Website";
+// console.log(moreName);
+
+// if(name) {
+//     console.log(message);
+//     let otherName = "This is a secret name";
+//     console.log(otherName);
+//     moreName = "Yes the content is there";
+//     console.log(moreName);
+// }
+// console.log(moreName);
+
+// // Create a Message using Prompttest
+// let name  = prompt("What is your name");
+// if(name === "Don") {
+//     console.log("Welcome to my site, Don")
+// }
+
+// // Other method
+let userName = prompt("What is your name?");
+let message = "Welcome to my site ";
+console.log(message + userName);
+
