a23a4aad0f6de35a03988faca05e9218a36f79be
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-10-05 14:33:35 +0530

    JS Operators

--- JS_core_fundamentals/lesson-1/chap.index.js
+++ JS_core_fundamentals/lesson-1/chap.index.js
@@ -1,101 +1,156 @@
-// let x =10;
-// console.log(x -5);
-// x = 20;
-// console.log(x + x);
-// let name = "Don Jude";
-// console.log(name)
-// name = "Akash";
-// console.log(name)
+let x =10;
+//console.log(x -5);
+x = 20;
+//console.log(x + x);
+let name = "Don Jude";
+//console.log(name)
+name = "Akash";
+//console.log(name)
 
-// // Challenge Part
-// var a = "hello world";
-// console.log(a);
-// a = "Welcome Back";
-// console.log(a);
+// Challenge Part
+var a = "hello world";
+////console.log(a);
+a = "Welcome Back";
+//console.log(a);
 
-// let myName = "Don Jude";
-// console.log(myName);
-// myName = "Rajashekaran";
-// console.log(myName);
+let myName = "Don Jude";
+//console.log(myName);
+myName = "Rajashekaran";
+//console.log(myName);
 
-// // // Challenge Part (Questions : 2)
-// var message;
-// console.log(message);
-// message = null;
-// console.log(message);
-// var myLight = false;
-// console.log(myLight);
-// myLight = true;
+// // Challenge Part (Questions : 2)
+var //////message;
+//console.log(//////message);
+//////message = null;
+//console.log(//////message);
+//var myLight = false;
+//console.log(myLight);
+myLight = true;
 
-// if(myLight === true) {
-//     console.log(myLight);
-// }
+if(myLight === true) {
+    //console.log(myLight);
+}
 
-// var score1, score2,score3,score4;
-// var a = "Hello";
-// var b = 10;
-// var c = false;
-// console.log(a);
+var score1, score2,score3,score4;
+var a = "Hello";
+var b = 10;
+var c = false;
+//console.log(a);
 
-// // Let message
-// let message;
-// console.log(message);
+// Let //test
+//let message;
+//console.log(//message);
 
-// message = null;
+//////message = null;
 
-// if(true) {
-//     let message = "updated message";
-//     console.log(message);
-// }
+if(true) {
+    //let message = "updated //message";
+    //console.log(/message);
+}
 
-// console.log(message)
+//console.log(//message)
 
-/// /// /// Lesson Challenge
+/ /// /// Lesson Challenge
 
-// let varA = "Don";
-// let varB = "Jude";
-// let varC = "Joseph"
+let varA = "Don";
+let varB = "Jude";
+let varC = "Joseph"
 
-// console.log(varA);
-// console.log(varB);
-// console.log(varC);
+//console.log(varA);
+//console.log(varB);
+//console.log(varC);
 
-/// /// /// Code Snippet
-// const test = true;
-// let message = "Welcome to Javascript";
-// let moreStuff;
-// if(test) {
-//     console.log(message);
-//     moreStuff = "Yes the content is there";
-//     let inBlock = "This is a secret";
-//     console.log(inBlock);
-// }
-// console.log(moreStuff)
+/// /// Code Snippet
+const test = true;
+//let message = "Welcome to Javascript";
+//let moreStuff;
+if(test) {
+    //console.log(//////message);
+    moreStuff = "Yes the content is there";
+    let inBlock = "This is a secret";
+    //console.log(inBlock);
+}
+//console.log(moreStuff)
 
 
-/// // /// Test Challenge
-// const name = true;
-// let moreName;
-// let message = "Welcome to the Website";
-// console.log(moreName);
+// /// Test Challenge
+//const name = true;
+let moreName;
+//let message = "Welcome to the Website";
+//console.log(moreName);
 
-// if(name) {
-//     console.log(message);
-//     let otherName = "This is a secret name";
-//     console.log(otherName);
-//     moreName = "Yes the content is there";
-//     console.log(moreName);
-// }
-// console.log(moreName);
+if(name) {
+    //console.log(//////message);
+    let otherName = "This is a secret name";
+    //console.log(otherName);
+    moreName = "Yes the content is there";
+    //console.log(moreName);
+}
+//console.log(moreName);
 
-// // Create a Message using Prompttest
-// let name  = prompt("What is your name");
-// if(name === "Don") {
-//     console.log("Welcome to my site, Don")
-// }
+// Create a //////message using Prompttest
+//let name  = prompt("What is your name");
+if(name === "Don") {
+    //console.log("Welcome to my site, Don")
+}
 
-// // Other method
+// Other method
 let userName = prompt("What is your name?");
-let message = "Welcome to my site ";
-console.log(message + userName);
+//let message = "Welcomdvdve to my site ";
+//console.log(//////message + userName);
+
+// Template literals
+//First example
+//let name = "Don Jude Joseph";
+//let message = `Welcome to Javascript ${name}`;
+//console.log(//message);
+
+//Second ExamDonple
+//let userName = prompt('"What is your name?');
+// let info = `welcome
+// ${userName}`;
+// //console.log(info);
+
+
+//Challenge on String
+const str = "Testing";
+//console.log(typeof str);
+
+
+// Lesson Challenge Code
+let myNum = "5";
+myNum = Number(myNum);
+//console.log(myNum);
+//console.log(typeof myNum);
+
+/// Fix the code
+// let a = Number('5');
+// let b = Number('10');
+//console.log(a + b);
+
+//Functions
+function echo(name, num) {
+
+    //console.log("Evaluating the " + name + " side");
+    return num;
+}
+// Notice the division operator (/)
+//console.log(echo("left", 6) / echo("right", 2));
+
+//Number and Testing the Operators
+let bar = 10
+let foo = 20;
+
+let fooC = bar += foo;
+//console.log(fooC)
+
+//Challenge Total sum
+let firstNum = 5;
+let secondNum = 10;
+firstNum ++; //10
+secondNum --; //0
+let total = firstNum + secondNum; //10 or 15
+console.log(total) //
+total = 500 + 100 / 5 + total;
+console.log(total);
 
